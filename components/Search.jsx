db6b1a2e43ae35a6ad1d63c8c98f05b9b8879b6a
Dariusz Kaminski <kaminski.darek@icloud.com>
2024-07-26 14:28:47 +0700

    new search bar

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,12 +1,29 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import Button from "./Button";
 
 export default function Search({ value, onChange }) {
   return (
     <>
-    <View>
-      <TextInput placeholder="search for a deck" onChangeText={(value) => onChange(value) }></TextInput>
-    </View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="search for a deck"
+          onChangeText={(value) => onChange(value)}
+        />
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100vw",
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#e0e1dd",
+    padding: 5,
+    paddingLeft: 10,
+    height: 40,
+  },
+});
