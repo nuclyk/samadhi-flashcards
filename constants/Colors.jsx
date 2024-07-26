62be8978f95bf0f82620d8a5deb19147e65c003b
Dariusz Kaminski <kaminski.darek@icloud.com>
2024-07-26 14:27:47 +0700

    colors as constants

---
+++ constants/Colors.jsx
@@ -0,0 +1,15 @@
+export const colors = {
+    primary : 
+    {
+      'color-50': '#e5f5ff',
+      'color-100': '#c9dbe9',
+      'color-200': '#aac1d3',
+      'color-300': '#8ba8c0',
+      'color-400': '#6c8fad',
+      'color-500': '#527593',
+      'color-600': '#3f5b74',
+      'color-700': '#2b4154',
+      'color-800': '#162735',
+      'color-900': '#000f18',
+    }
+}
