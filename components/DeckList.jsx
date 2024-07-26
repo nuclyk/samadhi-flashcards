fb067c1d708b40422fab97a087227c3935b97445
Dariusz Kaminski <kaminski.darek@icloud.com>
2024-07-26 14:28:11 +0700

    added list of decks

--- components/DeckList.jsx
+++ components/DeckList.jsx
@@ -1,3 +1,58 @@
+import { FlatList, Text, View, StyleSheet } from "react-native";
+import Deck from "../components/Deck";
+import { colors, primary } from "../constants/Colors";
+
 export default function DeckList({ decks }) {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={decks}
+        renderItem={({ item }) => (
+          <>
+            <View style={styles.deck}>
+              <View style={styles.title}>
+                <Text style={{fontSize: 20}}>{item.name}</Text>
+              </View>
+              <View style={styles.details}>
+                <Text>Due: {item.details.due}</Text>
+                <Text>New: {item.details.new}</Text>
+                <Text>Total: {item.details.total}</Text>
+              </View>
+            </View>
+          </>
+        )}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  deck: {
+    flex:1,
+    borderColor: colors.primary["color-500"],
+    borderWidth: 1,
+    margin: 10,
+    padding: 20,
+    justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 5
+  },
+  title: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginBottom: 15
+  },
+  details: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignContent: 'center'
+  }
+});
