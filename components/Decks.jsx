c8a57b416465688e39a0bbf0d6517d0f602620fd
Dariusz Kaminski <kaminski.darek@icloud.com>
2024-07-28 15:38:44 +0700

    component name change

---
+++ components/Decks.jsx
@@ -0,0 +1,81 @@
+import { FlatList, Text, View, StyleSheet, Pressable } from "react-native";
+import { colors } from "../constants/Colors";
+import { Link } from "expo-router";
+
+export default function Decks({ decks, filter }) {
+  return (
+    <FlatList
+      keyExtractor={(item) => item.id}
+      // Search for a deck by the filter param
+      data={decks.filter((deck) => deck.name.includes(filter))}
+      renderItem={({ item, index }) => (
+        <Link
+          push
+          style={[
+            styles.deck,
+            index % 2
+              ? { backgroundColor: colors["grey-light"] }
+              : { backgroundColor: colors.white },
+          ]}
+          // asChild allows to have something different as a link than only text
+          asChild
+          href={{
+            pathname: "/deck/[id]",
+            params: { id: item.id },
+          }}
+        >
+          <Pressable>
+            <View style={styles.title}>
+              <Text
+                style={{ fontSize: 15, color: colors.background }}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.name}
+              </Text>
+            </View>
+            <View style={styles.details}>
+              <View style={[styles.square, { backgroundColor: colors.orange }]}>
+                <Text style={styles.text}>Due: {item.details.due}</Text>
+              </View>
+              <View style={[styles.square, { backgroundColor: colors.blue }]}>
+                <Text style={styles.text}>New: {item.details.new}</Text>
+              </View>
+              <View style={[styles.square, { backgroundColor: colors.purple }]}>
+                <Text style={styles.text}>Total: {item.details.total}</Text>
+              </View>
+            </View>
+          </Pressable>
+        </Link>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  deck: {
+    backgroundColor: "white",
+    padding: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: colors["grey-medium"],
+  },
+  title: {
+    marginBottom: 15,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  square: {
+    justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 5,
+    padding: 5,
+    minWidth: 25,
+    marginRight: 5,
+  },
+  text: {
+    fontSize: 12,
+    color: colors.white,
+  },
+});
