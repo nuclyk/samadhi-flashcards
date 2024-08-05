51e3194897a0fdba6a4f53cda9bc212416cc3c64
Dariusz Kaminski <kaminski.darek@icloud.com>
2024-08-05 17:52:32 +0700

    Added tabs screen

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import Search from '@/components/Search';
+import AddDeck from '@/components/AddDeck';
+import DecksList from '@/components/DecksList';
+import { useState } from 'react';
+
+export default function Tab() {
+    const [query, setQuery] = useState('')
+    return (
+        <SafeAreaView>
+            <View style={{ flexDirection: 'row' }}>
+                <Search setQuery={setQuery} />
+                <AddDeck text='Add' deckName={query} />
+            </View>
+            <DecksList filter={query} />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
-import { DecksProvider } from "@/context/DecksContext";
-import { Stack } from "expo-router";
+import { DecksProvider } from '@/context/DecksContext';
+import { Stack } from 'expo-router/stack';
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ title: "Home" }} />;
-
+export default function Layout() {
+  return (
+    <DecksProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </DecksProvider>
+  );
 }
